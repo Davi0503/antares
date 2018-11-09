5aede65a94e9ca9c7d8ce05505a476d4d859065b
Davi Tavares Da Silva <44851410808@info.local>
2018-11-09 10:34:29 -0200

    criacao projeto

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Antares.vendas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
