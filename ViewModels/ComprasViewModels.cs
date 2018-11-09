c3b92b1981cf6fb7b4dcb34689e6ff4f104fa5af
Davi Tavares Da Silva <44851410808@info.local>
2018-11-09 15:35:03 -0200

    ComprasViewModels

--- ViewModels/ComprasViewModels.cs
+++ ViewModels/ComprasViewModels.cs
@@ -1,7 +1,16 @@
-namespace Antares.vendas.ViewModels
-{
-    public class ComprasViewModels
-    {
-        
+using System;
+using System.Collections.Generic;
+
+namespace Antares.vendas.ViewModels {
+    public class ComprasViewModels {
+
+        public int Id { get; set; }
+
+        public List<ProdutoViewModels> Produtos { get; set; }
+
+        public double TotalPreco { get; set; }
+
+        public DateTime DataCompra { get; set; }
+
     }
 }
--- ViewModels/ProdutoViewModels.cs
+++ ViewModels/ProdutoViewModels.cs
@@ -1,7 +1,10 @@
-namespace Antares.vendas.ViewModels
-{
-    public class ProdutoViewModels
-    {
-        
+namespace Antares.vendas.ViewModels {
+    public class ProdutoViewModels {
+
+        public string Id { get; set; }
+        public string Nome { get; set; }
+        public string PrecoUnidade { get; set; }
+        public string QuantidadeAtual { get; set; }
+
     }
 }
